49acc77da66643d218c1089b713172bd95539759
Sofia.Kipina@urfu.me <Sofia.Kipina@urfu.me>
2024-12-03 18:14:49 +0500

    Задание 10_2

---
+++ task10_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество экзаменов в сессии: ");
+            int k;
+            if (!TryInputNumber(out k))
+            {
+                Console.ReadKey();
+                return;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine("Введите балл за экзамен:");
+
+                int grade;
+                if (!TryInputNumber(out grade))
+                {
+                    
+                    Console.ReadKey();
+                    return;
+                }
+                sum += grade;
+            }
+
+            Console.WriteLine($"Средний балл за все экзамены:{sum/k}");
+
+            Console.ReadKey();
+        }
+
+        static bool TryInputNumber(out int number)
+        {
+            number = 0;
+            if (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Ошибка ввода");
+                return false;
+            }
+            return true;
+        }
+    }
+}
